8bd5926c59d78bf45247b478297673dbe4277865
parvinder-prog <palvihsp5@gmail.com>
2024-09-29 08:31:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs ,not browser
+
+//code readability that should be high
+
+let name = "Parvinder"
+let age = 18
+let isLoggedIn = false
+
+// different types of dataTypes(prempetive)
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
