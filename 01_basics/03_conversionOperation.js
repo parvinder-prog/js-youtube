0c4fc17cd3da52fcde22e94c5b024ecc48033e25
parvinder-prog <palvihsp5@gmail.com>
2024-09-30 14:38:45 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,14 +8,14 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN(not in number) but it's type is number
+// "33" => 33  it returns the number value which is 33
+// "33abc" => NaN(not in number) it returns NaN
 // true => 1; false => 0
 
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false
 // "" => false 
