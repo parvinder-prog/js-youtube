3cf9abb4db632d0aca1b221db25ffd9d2e6b9754
parvinder-prog <palvihsp5@gmail.com>
2024-10-02 08:04:30 +0000

    Operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33  it returns the number value which is 33
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false
 // "" => false 
@@ -24,6 +24,65 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***********************operations*********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // It returns the power of the given number like 3 is the power of 2
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Parvinder"
+let str2 = " Kaur" // give space in starting  the second word 
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1  + "2");
+// console.log("1" + 2 + 2);// if your 1st value is string then treats all vlaues in string
+// console.log(1 + 2 + "2");// if your last value is string then it adds your first values and then contact with string value 
+
+// console.log( (3 + 4) * 5 % 3);// so you use parenthesis in your code . its is the best method
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+// -----------------------------------------------
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+// link to study
+// https://tc39.es/ecma262/#sec-type-conversion
+
+
+
+
+
+
+
+
 
