8d2b4b68a825eea49d422213c4a96740a683d458
parvinder-prog <palvihsp5@gmail.com>
2024-10-06 09:12:45 +0000

    datatypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,38 +1,42 @@
-// # Primtive
+// # Primitive
 
-// 7 types : String , Number, Boolean, Null , undefined, Symbol(identifies unique value), BigInt
+// 7 types of primitiveDatatypes : String , Number, Boolean, Null , undefined, Symbol(identifies unique value), BigInt
 
 const score = 100
 const scoreValue = 100.3
+
 
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
 const id = Symbol('123')
-const anotherId = Symbol('123')
+const anotherId = Symbol('123')// Symbol returns unique values 
 
 console.log(id === anotherId);
 
-// const bigNumber = 65757463426536576869n
+ const bigNumber = 65757463426536576869n
 
 // Reference (Non primitive)
 
 //Array, Objects, Functions
 
-// arrays
+// *************************Arrays*************************
 const heros = ["shaktiman", "nagraj", "doga"] ;
 let myObj = {
     name: "palvi",
     age:22,
 }
-// Functions
+console.log(heros);
+
+// *********************Functions*************************
 const myFunction = function(){
-    console.log("Hello,World");
+     console.log("Hello World");
 }
 console.log(typeof bigNumber);
 console.log(typeof outsideTemp);// if we can check the typeof null then it returns object
 console.log(typeof myFunction);
+
 
 // basically non primitive data types always returns functions 
 // function returns called object function
