36feb3a3c0f03c7afd0928220dcf44f878fd72f8
parvinder-prog <palvihsp5@gmail.com>
2024-10-13 08:37:43 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "palvi"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value"); // it's outdated syntax
+// this below syntax is used 
+console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('parvinder')
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,4 +1,4 @@
-// # Primitive
+//  Primitive
 
 // 7 types of primitiveDatatypes : String , Number, Boolean, Null , undefined, Symbol(identifies unique value), BigInt
 
@@ -13,7 +13,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')// Symbol returns unique values 
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
  const bigNumber = 65757463426536576869n
 
@@ -27,16 +27,44 @@ let myObj = {
     name: "palvi",
     age:22,
 }
-console.log(heros);
+// console.log(heros);
 
 // *********************Functions*************************
 const myFunction = function(){
-     console.log("Hello World");
+    //  console.log("Hello World");
 }
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);// if we can check the typeof null then it returns object
-console.log(typeof myFunction);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);// if we can check the typeof null then it returns object
+// console.log(typeof myFunction);
 
 
 // basically non primitive data types always returns functions 
 // function returns called object function
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive) , Heap(Non-Primitive)
+
+//Stack memory returns only copy the value of a variable
+// Heap memory returns the reference of a original value
+
+let myYoutubename = "parvinderkaurdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "palvi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
