71e064c760a02419ee2ab333ea6241b6e5647e7d
parvinder-prog <palvihsp5@gmail.com>
2024-10-13 09:35:59 +0000

    strings done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,33 @@ const repoCount = 10
 // this below syntax is used 
 console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('parvinder')
+const gameName = new String('parvi-nder')
+
+// Functions of Strings
+console.log(gameName[8]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+
+const newString = gameName.substring(0, 4)// substring can't use negative values
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "     palvi    "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // Trim removes extra spaces in your string
+
+
+
+
+
+
+
+
+
