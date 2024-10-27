d422a514319f5c8b4a89e55fedfabb83151e3a3f
parvinder-prog <palvihsp5@gmail.com>
2024-10-27 14:12:11 +0000

    nums_and_math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,10 @@
 const name = "palvi"
 const repoCount = 10
 
-// console.log(name + repoCount + " Value"); // it's outdated syntax
+console.log(name + repoCount + " Value"); // it's outdated syntax
+
 // this below syntax is used:-
+
 console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
 
 //  *********************different types of Functions of Strings******************************
---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,25 @@
+ const score = 400
+//console.log(score);
+
+ const balance = new Number(100)
+ //console.log(balance);
+ 
+ //console.log(balance.toString()); // converts number into string 
+ //console.log(balance.toFixed(2)); // It returns how many decimal zeros after your value like (100.00)
+ 
+ const otherNumber = 123.8966
+ //console.log(otherNumber.toPrecision(4));
+
+ const hunderds = 1000000
+ //console.log(hunderds.toLocaleString('en-IN')); // it's by default returns the commmas in .U.S. standard but we can change then into Indian values standard then use('en-IN')
+ 
+ 
+ // +++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++ 
+ console.log(Math);
+ console.log(Math.abs(-4)); // abs math function converts negative values into positive values 
+ console.log(Math.round(4.6)); //
+ console.log(Math.ceil(4.9));
+ console.log(Math.floor(4.9));
+ 
+ 
+ 
